4dd478b7b6dbd1ce7a33282780873b49c102b67b
aqeel garner <aqeel.garner1@gmail.com>
2023-02-28 22:07:42 +0000

    questions for README

--- assets/index.js
+++ assets/index.js
@@ -5,7 +5,16 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    "What is the name of your project?",
+    "What was your motivation?", 
+    "Why did you build this project?", 
+    "What problem does it solve?",
+    "What did you learn?",
+    "What are the steps required to install your project?",
+    "Provide instructions and examples for use.",
+    "List your collaborators, if any, with links to their GitHub profiles.",
+    "What is the license you are using?",
+    "What tests have you done?"
 ];
 
 // function to write README file
